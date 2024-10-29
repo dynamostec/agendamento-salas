79edf80217357c41bb6902f6ddc5d519979b8403
VitorVieira <vitorhvvieira@gmail.com>
2024-10-29 10:32:52 -0300

    Começando o desenvolvimento do fluxo de salas com o controller

---
+++ back-end/src/camada_controller/sala.controller.ts
@@ -0,0 +1,40 @@
+import { Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { SalaDto } from "./dto/salaDto";
+
+
+@Controller('salas')
+export class SalaController {
+
+    constructor(private useCase:SalaUseCase){}
+
+
+    @Get()
+    listar():Array<SalaDto> {
+        return this.useCase.listar();
+    }
+
+    @Get('/administrador/:id')
+    consultarPorAdministrador(@Param('id') idAdministrador: number): SalaDto {
+        return this.useCase.consultarPorAdministrador(idAdministrador);
+    }
+
+    @Get('/:id')
+    consultarPorId(@Param('id') id: number): SalaDto {
+        return this.useCase.consultarPorId(id);
+    }
+
+    @Post()
+    cadastrar(novaSala: SalaDto): SalaDto {
+        return this.useCase.cadastrar(novaSala);
+    }
+
+    @Put('/:id') 
+    editar(novosDados: SalaDto, @Param('id') id: number): SalaDto {
+        return this.useCase.editar(novosDados, id);
+    }
+
+    @Delete('/:id')
+    deletar(@Param('id') id: number) {
+        this.useCase.deletar(id);
+    }
+ }
