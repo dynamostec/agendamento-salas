19774a2d1646db1eb7a5e620fb6ae5b595c35d64
DanielR1naldi <166469235+DanielR1naldi@users.noreply.github.com>
2024-11-02 17:37:57 -0300

    Alteracoes feitas tsxlogin e module

--- front-end/agendamento-salas/src/pages/Telas/login.tsx
+++ front-end/agendamento-salas/src/pages/Telas/login.tsx
@@ -1,9 +1,69 @@
+import { useState } from 'react';
 import styles from '../../styles/login.module.css';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [mensagemErro, setMensagemErro] = useState('');
+
+  // Função chamada ao enviar o formulário
+  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); 
+
+    // Exemplo de validação básica
+    if (email === '' || senha === '') {
+      setMensagemErro('Por favor, preencha todos os campos.');
+      return;
+    }
+
+    const dadosLogin = {
+      email,
+      senha
+    };
+
+    console.log(dadosLogin);
+
+    limparFormulario();
+  };
+
+  // Função para limpar os campos do formulário
+  const limparFormulario = () => {
+    setEmail('');
+    setSenha('');
+    setMensagemErro('');
+  };
+
   return (
-    <>
-      <h1>hello word</h1>
-    </>
+    <div className={styles.body}>
+      <div className={styles.container}>
+        <h2 className={styles.headerTitle}>Login</h2>
+        <p className={styles.headerDescription}>Digite os seus dados para realizar o login</p>
+        {mensagemErro && <p className={styles.errorMessage}>{mensagemErro}</p>} {/* Mensagem de erro */}
+        <form onSubmit={handleLogin}>
+          <div className={styles.formGroup}>
+            <input 
+              type="email" 
+              placeholder="Seu E-mail ..." 
+              value={email} 
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+              required 
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <input 
+              type="password" 
+              placeholder="Sua Senha ..." 
+              value={senha} 
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSenha(e.target.value)} 
+              required
+            />
+          </div>
+          <div className={styles.linkContainer}>
+            <a href="#" className={styles.forgotPassword}>Esqueci minha senha</a>
+          </div>
+          <button type="submit" className={styles.submitButton}>Entrar</button>
+        </form>
+      </div>
+    </div>
   );
 }
