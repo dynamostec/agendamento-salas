719d3bbac6fc3b3e59edd2345b35dc7e1b8ba9fb
HeitorHrecek <heitorhrecek@outlook.com>
2024-11-06 00:03:35 -0300

    Renderizar tela de mudar senha no componente Home

--- front-end/agendamento-salas/src/pages/index.tsx
+++ front-end/agendamento-salas/src/pages/index.tsx
@@ -1,10 +1,11 @@
 import Cadastro from "./Telas/cadastro"
 import Login from "./Telas/login"
+import MudarSenha from "./Telas/mudar-senha";
 
 export default function Home() {
   return (
     <>
-      <Login />
+      <MudarSenha />
     </>
   );
 }
