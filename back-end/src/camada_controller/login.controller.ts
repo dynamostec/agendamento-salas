9a23656a63c1fa5d3832e6028c28be4210e53594
André Gustavo <andregustavodeoliveira49@gmail.com>
2024-11-07 10:48:07 -0300

    finalização do fluxo-login

--- back-end/src/camada_controller/login.controller.ts
+++ back-end/src/camada_controller/login.controller.ts
@@ -1,10 +1,19 @@
-import { Controller } from "@nestjs/common";
-import { loginUseCase } from "src/camada_use_case/loguin.usecase";
+import { Controller, Post, Body, Res, HttpStatus } from '@nestjs/common';
+import { LoginUseCase } from 'src/camada_use_case/login.usecase';
 
 @Controller('login')
-export class loginController {
-   
-    constructor (private useCase: loginUseCase){}
+export class LoginController {
+  constructor(private loginUseCase: LoginUseCase) {}
 
-    
+  @Post('login')
+  async login(@Body() body, @Res() res) {
+    const { email, senha } = body;
+    const result = await this.loginUseCase.validar({ email, senha });
+
+    if (result.success) {
+      return res.status(HttpStatus.OK).json({ message: 'Login realizado com sucesso!' });
+    } else {
+      return res.status(HttpStatus.UNAUTHORIZED).json({ message: 'Credenciais inválidas' });
+    }
+  }
 }
