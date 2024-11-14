79312915e5671fef27c6602dd66f1bc450a63ea1
VitorVieira <vitorhvvieira@gmail.com>
2024-11-14 19:24:07 -0300

    Mudando porta onde o backend vai subir

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -5,6 +5,6 @@ import { HttpExceptionFilter } from './camada_controller/http.exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
