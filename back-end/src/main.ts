1433fe996923cbc154fb7ca52ce005e744c26e75
VitorVieira <vitorhvvieira@gmail.com>
2024-11-18 11:06:49 -0300

    Configurando CORS da API

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -5,6 +5,11 @@ import { HttpExceptionFilter } from './camada_controller/http.exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.enableCors({
+    origin: '*', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
   await app.listen(3001);
 }
 bootstrap();
