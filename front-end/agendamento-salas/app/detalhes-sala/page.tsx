2652ac089a6c05cdef51a7356da63a79552f23eb
VitorVieira <vitorhvvieira@gmail.com>
2024-11-22 14:59:41 -0300

    Começando a integração

--- front-end/agendamento-salas/app/detalhes-sala/page.tsx
+++ front-end/agendamento-salas/app/detalhes-sala/page.tsx
@@ -2,20 +2,55 @@
 
 import { useState } from 'react';
 import styles from '../../styles/detalhes-sala.module.css';
+import { useRouter } from 'next/router';
 
 export default function DetalhesSala() {
-  //exemplo de como pode ser puxado os dados:
+
+  const [id] = useState('');
+  const [nome] = useState('');
+  const [capacidade] = useState('');
+  const [cep] = useState('');
+  const [cidade] = useState('');
+  const [estado] = useState('');
+  const [rua] = useState('');
+  const [descricao] = useState('');
+  const router = useRouter();
+  // exemplo de como pode ser puxado os dados:
   // { dados }: { dados: any }
   // colocar dentro do p individual de cada um: <p> {dados.nome} {dados.capacidade} {dados.cep} {dados.cidade} {dados.estado} {dados.endereco} {dados.descricao} </p>
+
+
+  let dadosSala = {
+    id,
+    nome,
+    capacidade,
+    usuarioAdministrador: {
+      id: '',
+      nome: '',
+      email: '',
+      senha: '',
+      tipoUsuario: ''
+    },
+    localizacao: {
+      cep,
+      cidade,
+      estado,
+      rua,
+    },
+    descricao
+  };
+
   
-    return(
-      <div className={styles.page}>
+
+  return (
+    <div className={styles.page}>
       <div className={styles.container}>
         <h1 className={styles.titulo}>Detalhes da Sala</h1>
         <div className={styles.infoContainer}>
           <div className={styles.infoBasica}>
             <p><strong>Nome da sala:</strong> </p>
             <p><strong>Capacidade:</strong> </p>
+            <p><strong>Administrador:</strong></p>
             <p><strong>CEP:</strong> </p>
             <p><strong>Cidade:</strong> </p>
             <p><strong>Estado:</strong> </p>
@@ -28,5 +63,5 @@ export default function DetalhesSala() {
         </div>
       </div>
     </div>
-    )
+  )
 }
